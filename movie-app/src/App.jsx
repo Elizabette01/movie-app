ea01888a6b41e19bc1473a76136ed7b01d996cfd
Elizabeth Nduka <ndukaelizabeth26@gmail.com>
2025-07-29 08:06:03 +0100

    loading component done

--- movie-app/src/App.jsx
+++ movie-app/src/App.jsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from 'react'
 import hero from "/hero.png";
 import Search from "./Components/Search";
 import MovieCard from './Components/MovieCard';
+import LoadingCard from './Components/Loading';
 
 
 // Base API URL and API KEY
@@ -23,24 +24,49 @@ const App = () => {
 
   // useStates
   const [ searchTerm, setSearchTerm ] = useState('');
-  const [errorMessage, setErrorMessage] = useState('')
+  const [errorMessage, setErrorMessage] = useState('');
+  const [ movies, setMovies ] = useState([]);
+  const [loading, setLoading] = useState(false);
 
 
   // Function to make the API calls
   const fetchMovies = async () => {
+
+    setLoading(true)
+
     try {
       const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
-       const response = await fetch(endpoint, API_OPTIONS);
+      const response = await fetch(endpoint, API_OPTIONS);
+
+      if(!response.ok) {
+        throw new Error('Failed to fetch movies')
+      }
+
+      const data = await response.json();
+
+
+      if(data.Response === 'False'){
+        setErrorMessage( data.Error || 'Failed to fetch movies' );
+        setMovies([])
+        return;
+      }
+
+       setMovies(data.results || [])
+
     } catch (error) {
       console.error(`Error fetching movies: ${error}`);
       setErrorMessage('Error fetching movies. Please try again later.');
+
+
+    } finally {
+      setLoading(false)
     }
   }
 
 
   // useEffect to make our API call whenever the page loads
   useEffect(() => {
-
+    fetchMovies();
   }, []);
 
   return (
@@ -59,11 +85,24 @@ const App = () => {
       </div>
 
       <section className='all-movies'>
-        <h2 className='text-center'>All Movies</h2>
+        <h2 className='text-center mb-14 mt-7'>All Movies</h2>
 
-        <ul className='w-[85%] mx-auto'>
-        
-        </ul>
+         {loading ? (
+          <ul className="w-[85%] mx-auto grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-5 gap-10">
+            {Array.from({length: 20}).map((_, index) => (
+              <LoadingCard key={index} />
+            ))}
+          </ul>
+        ) : errorMessage ? 
+          ( <p className='text-purple-300 text-center'>{errorMessage}</p>
+          ) : (
+            <ul className='w-[85%] mx-auto'>
+              {movies.map(movie => (
+                <MovieCard key={movie.id} movie={movie}/>
+              ))}
+            </ul>
+          ) }
+
           
 
 
---
+++ movie-app/src/Components/Loading.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const LoadingCard = () => {
+  return (
+    <div className='flex flex-col gap-3'>
+      <div className="bg-dark-100 shadow-inner shadow-light-100/10  h-80 sm:w-60 rounded-xl animate-pulse"></div>
+      <div className="flex flex-col gap-1">
+         <div className="bg-dark-100 shadow-inner shadow-light-100/10 sm:w-60 animate-pulse h-5 rounded-md" ></div>
+          <div className="bg-dark-100 shadow-inner shadow-light-100/10  sm:w-60 animate-pulse h-3 rounded-md" ></div>
+      </div>
+      
+    </div>
+  )
+}
+
+export default LoadingCard

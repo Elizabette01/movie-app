da1464cfc8f4334905dc7cd8aeea07e20ee1fbdb
Elizabeth Nduka <ndukaelizabeth26@gmail.com>
2025-07-29 11:12:15 +0100

    getTrendingMovis function added

--- movie-app/src/appwrite.js
+++ movie-app/src/appwrite.js
@@ -53,4 +53,16 @@ const updateSearchCount = async (searchTerm, movie) => {
   }
 }
 
+
 export default updateSearchCount;
+
+
+export const getTrendingMovies = async () => {
+  try {
+    const result = await database.listDocuments(DATABASE_ID, COLLECTION_ID, [Query.limit(5), Query.orderDesc("count")])
+
+    return result.documents;
+  }catch(error) {
+    console.error(error);
+  }
+}
